7484f3a4d81d10b21b4ea652f6c00527a9f42914
F_Haga <s1240056@gmail.com>
2017-12-28 23:52:52 +0900

    To attach flake8

--- program/LVEZ1.py
+++ program/LVEZ1.py
@@ -12,24 +12,24 @@ class Dist():
         pass
 
     def Measure(self, gp):
-        GPIO_TRIGECHO = gp 
+        GPIO_TRIGECHO = gp
         GPIO.setup(GPIO_TRIGECHO, GPIO.OUT)
         GPIO.output(GPIO_TRIGECHO, False)
-        # This function measures a distance
-      # Pulse the trigger/echo line to initiate a measurement
+    # This function measures a distance
+    # Pulse the trigger/echo line to initiate a measurement
         GPIO.output(GPIO_TRIGECHO, True)
         time.sleep(0.00001)
         GPIO.output(GPIO_TRIGECHO, False)
-      #ensure start time is set in case of very quick return
+    # ensure start time is set in case of very quick return
         start = time.time()
 
-      # set line to input to check for start of echo response
+    # set line to input to check for start of echo response
         GPIO.setup(GPIO_TRIGECHO, GPIO.IN)
-        while GPIO.input(GPIO_TRIGECHO)==0:
+        while GPIO.input(GPIO_TRIGECHO) == 0:
             start = time.time()
 
-      # Wait for end of echo response
-        while GPIO.input(GPIO_TRIGECHO)==1:
+    # Wait for end of echo response
+        while GPIO.input(GPIO_TRIGECHO) == 1:
             stop = time.time()
 
         GPIO.setup(GPIO_TRIGECHO, GPIO.OUT)
@@ -39,6 +39,7 @@ class Dist():
         distance = (elapsed * 34300)/2.0
         time.sleep(0.1)
         return distance
+
 
 if __name__ == '__main__':
     LVEZ1 = Dist()
